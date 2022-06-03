cb34a36ca6fc554a586fcf14cf9cf05152c03761
Gulam Ansari <gulamansari57181@gmail.com>
2022-06-03 23:23:21 +0530

    Counter app implemented by using concept of state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return <div> Hello </div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>The Counter App Using State In React</h1>
+      </header>
+
+      <h2>
+        <strong>Current value of the counter is :</strong> <h1>{count}</h1>
+      </h2>
+
+      <button onClick={() => setCount(0)}>Reset the Counter</button>
+      <button onClick={() => setCount(count + 1)}>Increment the Counter</button>
+      <button onClick={() => setCount(count - 1)}>Decrement the Counter</button>
+    </div>
+  );
 }
 
 export default App;
